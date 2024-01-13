ebd7612c04bb3d410bbc7fc12c83a54984b119d8
P.Arthur <pedroarthurrodalm@gmail.com>
2024-01-13 15:46:00 -0300

    Dia 13/01/24

--- Aulas/teste.c
+++ Aulas/teste.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> //Permite entrada e saída de dados.
+#include <stdlib.h> //Permite a criação de variáveis.
+#include <locale.h> //Permite a utilização de acentos ortográficos em textos.
 
 void main(){
  //Imprime 'Olá mundo' na tela.
