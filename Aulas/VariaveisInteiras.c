f69cea98b115e76f4e8ec9de27b7ee5be5e13a01
P.Arthur <pedroarthurrodalm@gmail.com>
2024-01-13 17:37:33 -0300

    Variáveis Inteiras
    Fim do código da aula 13.

--- Aulas/VariaveisInteiras.c
+++ Aulas/VariaveisInteiras.c
@@ -3,5 +3,26 @@
 #include <locale.h>
 
 void main(){
-
+    setlocale(LC_ALL,"");
+    //Definindo uma variável.
+    int a = 5, b;
+    //Imprimindo a variável 'a'.
+    printf("%d", a);
+    //Concatenando.
+    printf("\nO valor da variável A é: %d", a);
+    //Mudando o valor de 'a'.
+    a = 15;
+    //concatenando.
+    printf("\nO valor de A agora é: %d", a);
+    //Lendo valores.
+    printf("\nEscolha o valor de B: ");
+    scanf("%d", &b);
+    //Concatenado.
+    printf("\nO valor de B é: %d", b);
+    //Pula linha.
+    printf("\n");
+    //Imprimindo.
+    printf("Fim!");
+    //Pausando.
+    system("pause");
 }
