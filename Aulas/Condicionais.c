92dae30a53ced75d6d0522e31aa1e8086e3cf1a1
P.Arthur <pedroarthurrodalm@gmail.com>
2024-01-15 19:31:04 -0300

    Código de Condicionais

--- Aulas/Condicionais.c
+++ Aulas/Condicionais.c
@@ -1,1 +1,42 @@
+#include <stdin.h>
+#include <stdlib.h>
+#include <locale.h>
 
+//Função principal do programa
+void main(){
+    setlocale(LC_ALL, "");
+    //Definindo Variáveis
+    int a = 4,opcao = 3;
+    float b = 2.5;
+    char c = 'x';
+
+    //Condicional Simples
+    if(a == 5){
+        printf("\n A variavel a = 5");
+    }
+    if(b == 2.5){
+        printf("\n A variavel b = 2.5");
+    }
+    if(c == 'x'){
+        printf("\n A variavel c = letra x");
+    }
+
+    //Número par ou impar
+    if(a % 2 == 1){
+        printf("\n A variavel a é impar");
+    }else{
+        printf("\n A variavel a é par");
+    }
+
+    //Condicional Composta
+    if(opcao == 1){
+        printf("\nA opcao = 1");
+    }else if(opcao == 2){
+         printf("\nA opcao = 2");
+    }else{
+        printf("\nA opcao nao é igual a 1 e nem 2");
+    }
+
+    //Pausa o programa após executar
+    system("pause");
+}
