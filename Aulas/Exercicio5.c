3daa85654ab8e093004621f7bbfacf45a95a0a2e
P.Arthur <pedroarthurrodalm@gmail.com>
2024-01-16 11:26:55 -0300

    Organizando o exercício 5

--- Aulas/Exercicio5.c
+++ Aulas/Exercicio5.c
@@ -5,7 +5,8 @@
 //Crie um algoritmo que leia 3 valores e informe se eles são
     //iguais entre si para formarem os lados de um triângulo equilátero.
 void main(){
-    setlocale(LC_ALL, "");
+    setlocale(LC_ALL, ""); //Habilitando acentuações.
+    //Definindo e lendo os lados informados.
     int ladoA, ladoB, ladoC;
     printf("Informe o comprimento do lado A: ");
     scanf("%d", &ladoA);
@@ -14,15 +15,18 @@ void main(){
     printf("Informe o comprimento do lado C: ");
     scanf("%d", &ladoC);
     printf("\n");
+    //Condicional para se o triândulo for equilátero.
     if (ladoA == ladoB && ladoB == ladoC){
         printf("O triângulo é equilátero");
     }
+    //Condicional para se o triândulo for isósceles.
     else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC){
         printf("O triângulo é isósceles");
     }
+    //Condicional para se o triândulo for escaleno.
     else{
         printf("O triângulo é escaleno");
     }
     printf("\n");
-system("pause");
+system("pause"); //Encerrando o sistema.
 }
