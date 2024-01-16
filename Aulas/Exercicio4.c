2e377949f7c15f6f753fe5692956a1a4a3153b9e
P.Arthur <pedroarthurrodalm@gmail.com>
2024-01-16 11:17:52 -0300

    Organizando o exercício 4

--- Aulas/Exercicio4.c
+++ Aulas/Exercicio4.c
@@ -7,22 +7,27 @@
     //senão foi reprovado.
 
 void main(){
-    setlocale(LC_ALL, "");
-    float n1, n2, n3, media;
+    setlocale(LC_ALL, ""); //Comando utilizado para abilitar acentuações.
+    float n1, n2, n3, media; //Definindo variáveis.
+
+    //Lendo e guardando as notas.
     printf("Digite a primeira nota: ");
     scanf("%f", &n1);
     printf("\nDigite a segunda nota: ");
     scanf("%f", &n2);
     printf("\nDigite a terceira nota: ");
     scanf("%f", &n3);
+
+    //Definindo a média do aluno e imprimindo na tela.
     media = (n1 + n2 + n3) / 3;
     printf("\nA média do aluno foi %.2f", media);
 
+    //Defindindo a condição para se o aluno foi ou não aprovado.
     if(media >= 7){
         printf("\nAluno aprovado!");
     }
     else{
         printf("\nAluno reprovado!\n");
     }
-system("pause");
+system("pause"); //Pausando o sistema
 }
