b0deccaca33e1c0905ac5e5f445d715b2df2b655
P.Arthur <pedroarthurrodalm@gmail.com>
2024-01-20 19:05:14 -0300

    Código da aula de Funções Vazias.

---
+++ Aulas/Ponteiros e Funções/FuncaoVoid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função que desenha borda
+void desenhaBorda(){
+     printf("|----| \n");
+}
+
+
+int main(){
+
+    //Chamando a Função
+    desenhaBorda();
+
+    //Imprimindo texto na tela
+    printf("hi\n");
+
+    //Chamando a Função
+    desenhaBorda();
+
+    return 0;
+}
